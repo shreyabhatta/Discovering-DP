bae97173257499c14fa0ba613e2381d6ecc9070d
Shreya Bhattacharya <84590255+shreyabhatta@users.noreply.github.com>
2022-06-13 05:08:33 +0530

    Update Knapsack Top down.cpp

--- 0-1 Knapsack/Knapsack Problem/Knapsack Top down.cpp
+++ 0-1 Knapsack/Knapsack Problem/Knapsack Top down.cpp
@@ -1,16 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int knapsack(int wt[], int val[], int n, int W)
 {
-	vector <vector <int> > t (n+1, vector <int> (W+1, 0));
+	int t[n+1][W+1];
+	memset (t, 0, sizeof(t));
 	
-	for(int i =1; i< n+1; i++)
+	for(int i=0; i<n+1; i++)
 	{
-		for(int j=1; j<W+1; j++)
+		for(int j=0; j<W+1; j++)
 		{
-			if (wt[i-1]> j) t[i][j] = t[i-1][j];
+			if(j==0||i==0)
+			{
+				t[i][j] =0;
+				continue;
+			}
+			
+			else if(wt[i]>W) t[i][j] = t[i-1][j];
 			
 			else t[i][j] = max((val[i-1]+ t[i-1][j-wt[i-1]]), t[i-1][j]);
 		}
