88051ae9ac455a9247d688787a7550e35a24d85b
Shreya Bhattacharya <84590255+shreyabhatta@users.noreply.github.com>
2022-06-13 04:09:34 +0530

    Update Minimum Subset sum diff.cpp

--- 0-1 Knapsack/Minimum Subset sum diff.cpp
+++ 0-1 Knapsack/Minimum Subset sum diff.cpp
@@ -2,42 +2,44 @@
 
 using namespace std;
 
-int MinSubsetDiff(int arr[], int n)
+int MinSubsetdiff(int arr[], int n)
 {
-	int sum1=0;
-	for(int k = 0; k<n; k++)
+	int sum = 0;
+	
+	for(int k=0; k<n; k++)
 	{
-		sum1+=arr[k];
+		sum+=arr[k];
 	}
-	int sum= sum1/2;
 	
 	int t[n+1][sum+1];
-	memset(t, 0 , sizeof(t));
-
-	while(sum)
+	memset(t, 0, sizeof(t));
+	
+	for(int i =0; i<n+1; i++)
 	{
-		for(int i =0; i<n+1; i++)
-		{
-			for(int j=0; j<sum+1; j++)
-			{
-                if (j==0) t[i][j] = 1;
+		for(int j=0; j<sum+1; j++)
+		{			
+			if (j==0) t[i][j] = 1;
+			if (i==0) continue;
 			
-                if(i==0) continue;
-                
-                if(arr[i-1]> j) t[i][j] = t[i-1][j];
-                
-                else t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j] ;
-			}
+			if (t[i-1][j]>sum) t[i][j] = t[i-1][j];
+			
+			else t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
 		}
-		if(t[n][sum] == true) return sum1 -2*sum;
-		sum--;			
 	}
-
+	
+	int l=sum/2;	
+	while(!t[n][l])
+	{
+		l--;
+	}
+	
+	return sum-2*l;
+	
 }
 
 int main()
 {
 	int arr[] = {1,2,7};
 	int n = sizeof(arr)/ sizeof(arr[0]);
-	cout << MinSubsetDiff(arr, n);
+	cout << MinSubsetdiff(arr, n);
 }
