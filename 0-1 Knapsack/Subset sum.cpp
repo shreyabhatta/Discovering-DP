65d40a4816b89bb372cf9b092266333d9c5af58a
Shreya Bhattacharya <84590255+shreyabhatta@users.noreply.github.com>
2022-07-15 23:10:28 +0530

    Update Subset sum.cpp

--- 0-1 Knapsack/Subset sum.cpp
+++ 0-1 Knapsack/Subset sum.cpp
@@ -8,20 +8,30 @@ bool SubsetSum(int arr[], int n, int sum)
     int t[n+1][sum+1];
     memset (t, false, sizeof(t));
     
-    for(int i =0; i<n+1; i++)
-    {
-        for(int j=0; j<sum+1; j++)
+        for(int i =0; i< n+1; i++)
         {
-                if (j==0) t[i][j] = 1;
-			
-                if(i==0) continue;
+            for(int j = 0; j<sum+1; j++)
+            {
+                if(i==0 || j==0)
+                {
+                    if(i==0)
+                    {
+                        t[i][j] = 0;
+                    }
+                    
+                    if(j==0)
+                    {
+                        t[i][j] = 1;
+                    }
+                    continue;
+                }
                 
-                if(arr[i-1]> j) t[i][j] = t[i-1][j];
+                if(arr[i-1]>j) t[i][j]  = t[i-1][j];
                 
-                else t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j] ;
+                else t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+            }
         }
-    }
-    return t[n][sum];	
+        return t[n][sum];	
 }
 
 int main()
