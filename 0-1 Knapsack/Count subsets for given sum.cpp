f8a7793914548e67272ab2236791550ca2ae9cf4
Shreya Bhattacharya <84590255+shreyabhatta@users.noreply.github.com>
2022-07-15 23:35:13 +0530

    Update Count subsets for given sum.cpp

--- 0-1 Knapsack/Count subsets for given sum.cpp
+++ 0-1 Knapsack/Count subsets for given sum.cpp
@@ -1,33 +1,25 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int CountSubsetSum(int arr[], int n, int sum)
-{
-	int t[n+1][sum+1];
-	memset(t, 0, sizeof(t));
-	
-	for(int i =0; i<n+1;i++)
-	{
-		for(int j=0; j<sum+1; j++)
-		{			
-			if (j==0) t[i][j] = 1;
-			
-			else if (i==0 ) t[i][j] = 0;
-				
-			else if (arr[i-1]> j) t[i][j] = t[i-1][j];
-			
-			else t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j] ;
-		}
-		
+int perfectSum(int nums[], int size, int target)
+	{   
+	    int n = size+1;
+        int m = target+1;
+        
+        vector<vector<int>> dp(n,vector<int> (m,0));
+        
+        for(int i=0;i<n;i++)
+            dp[i][0] = 1;
+        
+        for(int i=1;i<m;i++)
+            dp[0][i] = 0;
+        
+        for(int idx = 1;idx<n;idx++){
+            for(int sum = 0;sum<m;sum++){
+                
+                if(sum>=nums[idx-1])
+                    dp[idx][sum] = (dp[idx-1][sum - nums[idx-1]]% mod + dp[idx-1][sum]% mod) % mod;
+                else
+                    dp[idx][sum] = dp[idx-1][sum] % mod;
+            }
+        }
+        
+        return dp[n-1][m-1];
 	}
-	return t[n][sum];
-}
-
-int main() {
-	int set[] = {2,3,5,6,8,10};
-    int sum = 10;
-    int n = sizeof(set) / sizeof(set[0]);
-    cout << CountSubsetSum(set, n, sum);
-    return 0;
-}
