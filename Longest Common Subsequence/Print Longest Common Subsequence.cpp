eb3747307499979ede64f3ea185d39032cd5ca3e
Shreya Bhattacharya <84590255+shreyabhatta@users.noreply.github.com>
2022-07-17 22:13:01 +0530

    Update Print Longest Common Subsequence.cpp

--- Longest Common Subsequence/Print Longest Common Subsequence.cpp
+++ Longest Common Subsequence/Print Longest Common Subsequence.cpp
@@ -1,34 +1,36 @@
-string print_lcs(string x, string y, int n, int m)
-{
+vector<int> longestCommonSubsequence(vector<int> a, vector<int> b) {
+    int n = a.size(), m = b.size();
     int t[n+1][m+1];
     memset(t, 0, sizeof(t));
     
     for(int i =1; i<n+1; i++)
     {
-        for(int j=1; j<m+1; j++)
-        {                
-            if(x[i-1]==y[j-1]) t[i][j] = 1 + t[i-1][j-1];
+        for(int j = 1; j<m+1; j++)
+        {
+            if(a[i-1]==b[j-1]) t[i][j] = 1 + t[i-1][j-1];
             
-            else t[i][j] = max(t[i-1][j],t[i][j-1]);
+            else t[i][j] = max(t[i-1][j], t[i][j-1]);
         }
     }
     
-    int i=n, j=m;
-    string s ="";
+    int i = n, j = m;
+    vector<int> v;
+    
     while(i>0 && j>0)
     {
-        if(x[i-1]==y[j-1])
+        if(a[i-1]==b[j-1])
         {
-            s.push_back(x[i-1]);
+            v.push_back(a[i-1]);
             i--;
             j--;
-        }
+        } 
         else
         {
             if(t[i-1][j]>t[i][j-1]) i--;
             else j--;
         }
     }
-  reverse(s.begin(), s.end());
-  return s;
+    
+    reverse(v.begin(), v.end());
+    return v;
 }
