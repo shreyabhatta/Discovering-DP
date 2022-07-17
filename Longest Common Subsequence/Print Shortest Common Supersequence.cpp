3c8c01c4939585ddb675e687be1b7faf7c3623f9
Shreya Bhattacharya <84590255+shreyabhatta@users.noreply.github.com>
2022-07-17 22:53:17 +0530

    Create Print Shortest Common Supersequence.cpp

---
+++ Longest Common Subsequence/Print Shortest Common Supersequence.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.length(), m = str2.length();
+        int t[n+1][m+1];
+        memset(t, 0, sizeof(t));
+
+        for(int i =1; i<n+1; i++)
+        {
+            for(int j=1; j<m+1; j++)
+            {                
+                if(str1[i-1]==str2[j-1]) t[i][j] = 1 + t[i-1][j-1];
+
+                else t[i][j] = max(t[i-1][j],t[i][j-1]);
+            }
+        }
+        
+        string s = "";
+        int i = n, j = m;
+        
+        while(i>0 && j>0)
+        {
+            if(str1[i-1]==str2[j-1]) 
+            {
+                s = str1[i-1] + s;
+                i--;
+                j--;
+            }
+            
+            else
+            {
+                if(t[i-1][j]> t[i][j-1]) 
+                {
+                    s = str1[i-1] + s;
+                    i--;
+                }
+                
+                else
+                {
+                    s = str2[j-1] + s;
+                    j--;
+                }
+            }
+        }
+        
+        while(i>0)
+        {
+            s = str1[i-1] + s;
+            i--;
+        }
+        
+        while(j>0)
+        {
+            s = str2[j-1] + s;
+            j--;
+        }
+        
+        return s;
+    }
+};
